db8d7c62a403cacff5536c96b1e5ae70d09d7fa6
Davide Agosti <davide.a@davideagosti.info>
2019-10-25 11:55:00 +0100

    Add errors to Login and Register controllers

--- models/user.model.js
+++ models/user.model.js
@@ -121,14 +121,14 @@ UserSchema.statics.findByIdAndToken = function(_id, token) {
 UserSchema.statics.findByCredentials = function(email, password) {
 	let User = this;
 	return User.findOne({ email }).then(user => {
-		if (!user) return Promise.reject();
+		if (!user) return Promise.reject('User not found');
 
 		return new Promise((resolve, reject) => {
 			bcrypt.compare(password, user.password, (err, res) => {
 				if (res) {
 					resolve(user);
 				} else {
-					reject();
+					reject('Password incorrect');
 				}
 			});
 		});
